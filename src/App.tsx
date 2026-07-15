046003dd0989481e76ed70670ba4a52cd55b1dae
Jorge Donoso <jdevcl@gmail.com>
2026-07-15 12:39:24 -0700

    Add sentence skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { SentenceSkeleton } from "./components/SentenceSkeleton";
+
 function App() {
-  return <h1>Skeleton Loaders</h1>;
+  return (
+    <div className="max-w-3xl mx-auto flex flex-col items-center gap-4 mt-4">
+      <h1 className="text-4xl text-zinc-100">Skeleton Loaders</h1>
+      <h2 className="text-2xl text-zinc-100">Sentence</h2>
+      <SentenceSkeleton />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SentenceSkeleton.tsx
@@ -0,0 +1,12 @@
+export function SentenceSkeleton() {
+  return (
+    <div className="flex w-full flex-col items-center gap-4 rounded bg-zinc-800 p-4">
+      <div className="w-full animate-pulse space-y-4">
+        <div className="mx-auto h-8 w-32 rounded bg-zinc-600" />
+        <div className="mx-auto h-24 w-3/4 rounded bg-zinc-600" />
+        <div className="mx-auto h-4 w-1/2 rounded bg-zinc-600" />
+        <div className="h-4 w-full rounded bg-zinc-600" />
+      </div>
+    </div>
+  );
+}
